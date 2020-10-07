52c03d34bb7a554c00c78ec99b31079a09d6fde3
Jackie <50996868+JackiePeng666@users.noreply.github.com>
2020-10-07 13:14:51 +0800

    加了singeton的测试，运行成功

---
+++ day02/src/com.atguigu.ioc.scope/Test01.java
@@ -0,0 +1,15 @@
+package com.atguigu.ioc.scope;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext ac=new ClassPathXmlApplicationContext("scope.xml");
+        Student student1 = ac.getBean("student", Student.class);
+        Student student2 = ac.getBean("student", Student.class);
+        System.out.println(student1);
+        System.out.println(student2);
+
+    }
+}
