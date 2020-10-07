e85d61fad84c4a839eefd4241e0a2034adccf54d
Jackie <50996868+JackiePeng666@users.noreply.github.com>
2020-10-07 16:20:41 +0800

    关闭bean

---
+++ day02/src/com.atguigu.ioc.life/Test01.java
@@ -0,0 +1,16 @@
+package com.atguigu.ioc.life;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.SystemPropertyUtils;
+
+public class Test {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext ac=new ClassPathXmlApplicationContext("life.xml");
+        Person person = ac.getBean("person", Person.class);
+        System.out.println(person);
+        ac.close();
+
+    }
+}
+
