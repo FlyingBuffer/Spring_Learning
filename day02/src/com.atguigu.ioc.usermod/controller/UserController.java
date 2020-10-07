63cb047cb827feb102994e2f4fb07bf03f89ccc1
Jackie <50996868+JackiePeng666@users.noreply.github.com>
2020-10-07 23:42:28 +0800

    控制层

---
+++ day02/src/com.atguigu.ioc.usermod/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.atguigu.ioc.usermod.controller;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+    public UserController(){
+        System.out.println("UserController");
+    }
+
+}
