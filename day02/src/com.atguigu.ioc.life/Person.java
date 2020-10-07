3f4d956b94da1b56bbeaed52285b993780592c43
Jackie <50996868+JackiePeng666@users.noreply.github.com>
2020-10-07 16:11:47 +0800

    Update Person.java

--- day02/src/com.atguigu.ioc.life/Person.java
+++ day02/src/com.atguigu.ioc.life/Person.java
@@ -1,1 +1,49 @@
+package com.atguigu.ioc.life;
+
+public class Person {
+    private Integer id;
+    private String sex;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        System.out.println("Two:依赖注入");
+        this.id = id;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Person() {
+        System.out.println("One:创建对象");
+    }
+
+    @Override
+    public String toString() {
+        return "Four:使用";
+    }
+
+    public void  init(){
+        System.out.println("Three:初始化");
+    }
+    public void destroy(){
+        System.out.println("Five:销毁");
+    }
+}
 
