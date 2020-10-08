442fe69423146889cabe58a212f09a4b24c946ca
Jackie <50996868+JackiePeng666@users.noreply.github.com>
2020-10-08 23:09:08 +0800

    Create ProxyUtil02.java

---
+++ day02/src/com.atguigu.proxy/ProxyUtil02.java
@@ -0,0 +1,26 @@
+package com.atguigu.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class ProxyUtil {
+    private MathImpl mathImpl;
+
+    public ProxyUtil(MathImpl mathImpl) {
+        this.mathImpl = mathImpl;
+    }
+     public Object getProxy(){
+        ClassLoader loader=this.getClass().getClassLoader();
+        Class[] interfaces=mathImpl.getClass().getInterfaces();
+        return Proxy.newProxyInstance(loader, interfaces, new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                System.out.println("前");
+                Object result = method.invoke(mathImpl,args);//动态代理对象实现功能
+                System.out.println("后");
+                return result;
+            }
+        });
+    }
+}
