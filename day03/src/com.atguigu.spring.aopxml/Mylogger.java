3e077365c7f149d1c8c3ae2a0001579a9bf5709e
Jackie <50996868+JackiePeng666@users.noreply.github.com>
2020-10-10 00:29:14 +0800

    Create Mylogger.java

---
+++ day03/src/com.atguigu.spring.aopxml/Mylogger.java
@@ -0,0 +1,10 @@
+package com.atgui.spring.aopxml;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyLogger {
+    public void before(){
+        System.out.println("前置通知");
+    }
+}
