1fac8aade3360c029cdfa2a53390fc149bf05bec
Jackie <50996868+JackiePeng666@users.noreply.github.com>
2020-10-10 00:28:51 +0800

    Create Test.java

---
+++ day03/src/com.atguigu.spring.aopxml/Test.java
@@ -0,0 +1,13 @@
+package com.atgui.spring.aopxml;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext ac=new ClassPathXmlApplicationContext("aop-xml.xml");
+        MathI math = ac.getBean("mathImpl", MathI.class);
+        int add = math.add(12, 12);
+        System.out.println(add);
+    }
+}
