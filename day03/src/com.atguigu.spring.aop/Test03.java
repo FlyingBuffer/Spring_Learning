5b0fac06679450dfcea2ee37765a8ef593c80049
Jackie <50996868+JackiePeng666@users.noreply.github.com>
2020-10-09 19:12:28 +0800

    测试成功

---
+++ day03/src/com.atguigu.spring.aop/Test03.java
@@ -0,0 +1,15 @@
+package com.atguigu.spring.aop;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext ac=new ClassPathXmlApplicationContext("aop.xml");
+        MathI math=ac.getBean("mathImpl",MathI.class);
+        int i=math.sub(1,2);
+        System.out.println(i);
+        TestHandler bean = ac.getBean("testHandler", TestHandler.class);
+        bean.test();
+    }
+}
