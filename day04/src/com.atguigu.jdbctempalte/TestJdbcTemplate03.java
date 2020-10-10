6dc5fff5c9f179d0161ac6a05f16a0d31e7133f4
Jackie <50996868+JackiePeng666@users.noreply.github.com>
2020-10-10 15:53:29 +0800

    测试成功，刷新数据库

---
+++ day04/src/com.atguigu.jdbctempalte/TestJdbcTemplate03.java
@@ -0,0 +1,16 @@
+package com.atguigu.jdbctempalte;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class TestJdbcTemplate {
+    ApplicationContext ac=new ClassPathXmlApplicationContext("jdbc.xml");
+    JdbcTemplate jdbcTemplate = ac.getBean("jdbcTemplate", JdbcTemplate.class);
+    @Test
+    public void test(){
+        //jdbcTemplate.update("insert into emp values(null,'张三',23,'男')");
+        String sql="insert into emp values(null,?,?,?)";
+        jdbcTemplate.update(sql,"李四",24,"女");
+    }
+}
