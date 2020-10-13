7bf72bf51f417a8dbbd37f04bd19dd50a047b6c1
Jackie <50996868+JackiePeng666@users.noreply.github.com>
2020-10-13 23:55:57 +0800

    加了timeout

---
+++ day04/src/com.atguigu.book/Service/impl/BookServiceImpl05.java
@@ -0,0 +1,48 @@
+package com.atguigu.book.service.impl;
+
+import com.atguigu.book.dao.BookDao;
+import com.atguigu.book.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookDao dao;
+    /*
+    * @Transactional：对方法中所有的操作作为一个事务进行管理
+    * 在方法上使用只对方法有效果
+    * 在类上使用，对类中所有的方法都有效果
+    * @Transactional中可以设置的属性:
+    * propagation:A和B方法都有事务，当A在调用B时，会将A中的事务传播给B方法，B方法对于事务的处理方式就是事务的传播行为
+    * Propagation.REQUIRED:必须使用调用者的事务
+    * REQUIRES_NEW:将调用者的事务挂起，不使用调用者的事务，使用新的事务进行处理
+    *
+    * isolation:事务的隔离级别,在并发的情况下，操作数据的一种规定
+    *            读未提交:脏读 1
+     *           读已提交:不可重复读 2
+     *           可重复读:幻读 4
+     *           串行化:性能低，消耗大 8
+    *
+    *
+    * timeout:在事务强制回滚前最多可以执行(等待)的时间
+    * readOnly:
+    * rollbackFor|rollbackForClassName|noRollbackFor|noRollbackForClassName
+    * */
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW,timeout = 3)
+    public void buyBook(String bid, String uid) {
+        try{
+            Thread.sleep(5000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        Integer price = dao.selectPrice(bid);
+        dao.updateSt(bid);
+        dao.updateBalance(uid,price);
+    }
+}
